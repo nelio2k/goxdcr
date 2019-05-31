25f1e91254c8cf8bdc769ee18dc17e651799a7a6
Neil Huang <neil.huang@couchbase.com>
2019-05-31 14:55:56 -0700

    MB-32895 - Make pcre enabled by default on all platforms
    Change-Id: I0092bb3883414373b0e97b5cd6d1e68829c66a09

--- base/simple_utils_test.go
+++ base/simple_utils_test.go
@@ -1,5 +1,3 @@
-// +build !pcre
-
 package base
 
 import (
@@ -307,4 +305,35 @@ func TestLegacyKeyMsg(t *testing.T) {
 	err = ValidateAdvFilter("int LIKE 0")
 	assert.Equal(ErrorFilterInvalidExpression, err)
 	fmt.Println("============== Test case end: TestLegacyKeyMsg =================")
+}
+
+func TestSkipXattrAndStringsConversion(t *testing.T) {
+	fmt.Println("============== Test case start: TestSkipXattrAndStringsConversion =================")
+	assert := assert.New(t)
+	InitPcreVars()
+
+	userFilter := "META().id = \"something\" AND META().xattrs.testXattrKey EXISTS"
+	assert.True(FilterContainsXattrExpression(userFilter))
+	assert.True(FilterContainsKeyExpression(userFilter))
+
+	filterExpressionInternal := ReplaceKeyWordsForExpression(userFilter)
+
+	assert.False(strings.Contains(filterExpressionInternal, ExternalKeyXattrContains))
+	assert.False(strings.Contains(filterExpressionInternal, ExternalKeyKeyContains))
+	assert.False(strings.Contains(filterExpressionInternal, "META()"))
+
+	filterExpressionExternal := ReplaceKeyWordsForOutput(filterExpressionInternal)
+	assert.True(strings.Contains(filterExpressionExternal, "META()"))
+	// No escaped (i.e. no "META\\(\\)" )
+	assert.False(strings.Contains(filterExpressionExternal, ExternalKeyXattr))
+	assert.False(strings.Contains(filterExpressionExternal, ExternalKeyKey))
+	assert.True(strings.Contains(filterExpressionExternal, ExternalKeyXattrContains))
+	assert.True(strings.Contains(filterExpressionExternal, ExternalKeyKeyContains))
+
+	userFilter = fmt.Sprintf("REGEXP_CONTAINS(`%v`, \"^d\")", InternalKeyKey)
+	assert.False(strings.Contains(userFilter, "META()"))
+	userFilter = ReplaceKeyWordsForOutput(userFilter)
+	assert.True(strings.Contains(filterExpressionExternal, "META()"))
+
+	fmt.Println("============== Test case end: TestSkipXattrAndStringsConversion =================")
 }
