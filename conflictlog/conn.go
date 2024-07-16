d2787f598eaeefe8b2b4cfd29b1428b10088fedb
Sumukh Bhat <sumukh.bhat@couchbase.com>
2024-07-17 02:05:20 +0530

    MB-62742: fix because of pool changes

--- conflictlog/conn.go
+++ conflictlog/conn.go
@@ -7,5 +7,5 @@ import (
 type Connection interface {
 	io.Closer
 	SetMeta(key string, val []byte, dataType uint8, target Target) (err error)
-	SetMetaObj(key string, obj interface{}) (err error, target Target)
+	SetMetaObj(key string, obj interface{}, target Target) (err error)
 }
